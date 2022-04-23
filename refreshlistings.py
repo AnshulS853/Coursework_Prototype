05137a3c56b166bf042e7a008d1edf3edb3591c3
anshul <singhanshul44@gmail.com>
2022-04-23 14:17:54 +0100

    updates files

--- refreshlistings.py
+++ refreshlistings.py
@@ -6,23 +6,32 @@ class refreshLists:
         self.app = app
         self.conn = sqlite3.connect("auc_database.db", isolation_level=None)
         self.cur = self.conn.cursor()
-        self.today = datetime.now().date()
         self.refreshall()
 
     def refreshall(self):
-        self.cur.execute('SELECT listingID FROM listings WHERE dateofend=? AND active=1', (self.today,))
-        result = self.cur.fetchall()
+        self.cur.execute("SELECT listingID FROM listings WHERE dateofend<=DATE('now') AND active=1 AND format = 'Buy Now'")
+        buynowresult = self.cur.fetchall()
 
-        insertlist = []
+        self.cur.execute("SELECT listingID FROM listings WHERE dateofend<=DATE('now') AND active=1 AND format = 'Auction'")
+        auctionresult = self.cur.fetchall()
 
-        for i in result:
+        buynowlist = []
+        auctionlist = []
+
+        for i in buynowresult:
             if i is not None:
                 i = i[0]
-                insertlist.append(i)
+                buynowlist.append(i)
 
-        self.updatedatabase(insertlist)
+        for j in auctionresult:
+            if j is not None:
+                j = j[0]
+                auctionlist.append(j)
 
-    def updatedatabase(self,list):
+        self.updatebuynow(buynowlist)
+        self.updateauctions(auctionlist)
+
+    def updatebuynow(self, list):
         for i in list:
             currentlistingID = i
             self.cur.execute('''
@@ -30,3 +39,27 @@ class refreshLists:
                             SET active=0
                             WHERE listingID = (?)
                             ''',(currentlistingID,))
+
+    def updateauctions(self,list):
+        for i in list:
+            currentlistingID = i
+            self.cur.execute('''SELECT bidderID,highestBid FROM auctions WHERE listingID = ?''',(currentlistingID,))
+            result = self.cur.fetchall()
+            if result:
+                result = result[0]
+                self.cur.execute('''
+                                INSERT INTO invoice
+                                (listingID,
+                                buyerID,
+                                total,
+                                purchasedate)
+                                VALUES (?,?,?,DATE('now'))
+                                ''',(currentlistingID,result[0],result[1]))
+
+            self.cur.execute('''
+                            UPDATE listings
+                            SET active=0
+                            WHERE listingID = (?)
+                            ''', (currentlistingID,))
+
+
